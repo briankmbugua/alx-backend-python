01c94af8724c0fcb9422d5f055c63e60f8321cab
briankmbugua <briankmbuguak@gmail.com>
2023-10-09 22:45:04 +0300

    corrections

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -7,11 +7,5 @@ import math
 def floor(n: float) -> int:
     """
     Calculate the floor of a floating-point number.
-
-    Args:
-        n (float): The input floating-point number.
-
-    Returns:
-        int: The floor of the input number as an integer.
     """
     return math.floor(n)
