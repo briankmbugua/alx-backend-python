fcba86fee4a42c3c978ca6c978b887c61cd684e4
briankmbugua <briankmbuguak@gmail.com>
2023-10-10 02:22:55 +0300

    complex types

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Returns the sum of a list of floating-point numbers.
+'''
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''Returns the sum of a list of floating-point numbers.
+    '''
+    return float(sum(input_list))
