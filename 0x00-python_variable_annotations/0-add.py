380efb5e89d9248b50a6da7f065b7b03db1eb02e
briankmbugua <briankmbuguak@gmail.com>
2023-10-09 22:17:17 +0300

    made formatting changes using pycodstyle

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+"""
+A module to add two flaoting point numbers and return float
+"""
+
+
 def add(a: float, b: float) -> float:
     """
     Add two floating point numbers and return their sum as float
